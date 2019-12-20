d0b70b3cf9c1a9fde49aaa4974cf87e82422fc58
TheOriginalAse <rishiryan4@gmail.com>
2019-12-20 15:12:10 -0500

    template

---
+++ main.js
@@ -0,0 +1,48 @@
+// Scroll when clicking navbar sections
+function ScrollTo(name) {
+    ScrollToResolver(document.getElementById(name));
+  }
+  
+  function ScrollToResolver(elem) {
+    var jump = parseInt(elem.getBoundingClientRect().top * .5);
+    document.body.scrollTop += jump;
+    document.documentElement.scrollTop += jump;
+    if (!elem.lastjump || elem.lastjump > Math.abs(jump)) {
+      elem.lastjump = Math.abs(jump);
+      setTimeout(function() { ScrollToResolver(elem);}, "40");
+    } else {
+      elem.lastjump = null;
+    }
+  }
+
+// Animating fade ins when scrolled to element
+  var animateHTML = function() {
+    var elems;
+    var windowHeight;
+    function init() {
+      elems = document.querySelectorAll('.hidden');
+      windowHeight = window.innerHeight;
+      addEventHandlers();
+      checkPosition();
+    }
+    function addEventHandlers() {
+      window.addEventListener('scroll', checkPosition);
+      window.addEventListener('resize', init);
+      window.addEventListener('load', init);
+    }
+    function checkPosition() {
+      for (var i = 0; i < elems.length; i++) {
+        var positionFromTop = elems[i].getBoundingClientRect().top;
+        if (positionFromTop - windowHeight <= 0) {
+          elems[i].className = elems[i].className.replace(
+            'hidden',
+            'fade-in-element'
+          );
+        }
+      }
+    }
+    return {
+      init: init
+    };
+  };
+  animateHTML().init();
